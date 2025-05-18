f454f0cb0af3f910919891dafa087049ac3928e6
А <116899834+annistasiuk@users.noreply.github.com>
2025-05-18 11:49:03 +0300

    Delete songs directory

--- songs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-admin.site.register(models.Category)
-admin.site.register(models.Playlist)
-admin.site.register(models.Genre)
-admin.site.register(models.Song)
